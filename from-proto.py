ebb474ee7b54aeef9f99f684a3cb0c1c66c52d46
Rémy Frèrebeau <remy.frerebeau@systemathics.com>
2021-01-28 12:03:33 +0100

    local
    virtualenv -p python3.8 `pwd`
    
    from-proto.py
    
    fix
    
    add logging
    
    rename __init__.py files
    
    ok
    
    fix
    
    from-proto
    
    publish package

---
+++ from-proto.py
@@ -0,0 +1,166 @@
+import os
+import subprocess
+
+def create_dir_if_needed(dir):
+    if(not(os.path.isdir(dir))):
+        os.makedirs(dir)
+
+def delete_dir_if_needed(top_dir):
+    if(os.path.isdir(top_dir)):
+        for root, dirs, files in os.walk(top_dir, topdown=False):
+            for file in files:
+                os.remove(os.path.join(root, file))
+            for dir in dirs:
+                os.rmdir(os.path.join(root, dir))
+        os.rmdir(top_dir)
+
+def is_grpc_file (path):
+    """Test if a *.proto file describe a grpc service"""
+    for line in open(path, 'r').readlines() :
+        if line.startswith('service ') :
+            return True
+    return False
+
+def sort_proto_files(dir):
+    """Used to sort *.proto files under a directory `dir` in 2 categories :
+    - grpc files (containing a a grpc service)
+    - proto files (without a a grpc service)
+
+    Returns a tuple with 2 lists. 
+    First list containts grpc files. 
+    Second list containt regular proto files
+    """
+    grpc_files = []
+    proto_files = []
+    for root, dirs, files in os.walk(dir, topdown=True):
+        # skip google folder
+        if 'google' in dirs:
+            dirs.remove('google')
+        for file in files:
+            path = os.path.join(root, file)
+            if (file.endswith('.proto')) :
+                if(is_grpc_file(path)) :
+                    grpc_files.append(path)
+                else:
+                    proto_files.append(path)
+    return (grpc_files,proto_files)
+
+def generate_grpc_file(files, scr_dir, target_dir):
+    create_dir_if_needed(target_dir)
+    all_files= " ".join(files)
+    cmd = f"python3 -m grpc_tools.protoc --proto_path={scr_dir} --python_out={target_dir} --grpc_python_out={target_dir} {all_files}"
+    print(cmd)
+    subprocess.call(cmd, cwd=scr_dir, shell=True)
+
+def generate_proto_file(files, scr_dir, target_dir):
+    all_files= " ".join(files)
+    create_dir_if_needed(target_dir)
+    cmd = f"python3 -m grpc_tools.protoc --proto_path={scr_dir} --python_out={target_dir} {all_files}"
+    print(cmd)
+    subprocess.call(cmd, cwd=scr_dir, shell=True)
+
+def generate(tuple_of_list, src_dir, target_dir):
+    grpc_files = tuple_of_list[0]
+    proto_files = tuple_of_list[1]
+
+    generate_grpc_file(grpc_files, src_dir, target_dir)
+    generate_proto_file(proto_files, src_dir, target_dir)
+
+def add_namespace_import_to_init_py(dir):
+    for root, _, _ in os.walk(dir, topdown=True):
+        path = os.path.join(root,'__init__.py')
+        with open(path, 'w') as f:
+            f.write("__import__('pkg_resources').declare_namespace(__name__)")
+
+def increment_ver(version):
+    version = version.split('.')
+    version[2] = str(int(version[2]) + 1)
+    result = '.'.join(version)
+    return result
+
+def extract_version(data):
+    version_pattern ='version="'
+    l = len(version_pattern)
+    start = data.find('version="')
+    end = data.find('"', start + l + 1)
+    return data[start + l : end]
+
+def replace_version_in_setup(dir):
+    # Read in the file
+    path = os.path.join(dir,'setup.py')
+    with open(path, 'r') as file :
+        filedata = file.read()
+
+    old_version = extract_version(filedata)
+    version_from_env = os.getenv('VERSION', "")
+
+    if(version_from_env == ""):
+        new_version= increment_ver(old_version)
+    else:
+        new_version = version_from_env
+
+    # Replace the target string
+    filedata = filedata.replace(old_version, new_version)
+
+    # Write the file out again
+    with open(path, 'w') as file:
+        file.write(filedata)
+
+def generate_package(root_dir):
+    cmd = "python3 setup.py sdist"
+    subprocess.call(cmd, cwd=root_dir, shell=True)
+
+def publish_package(root_dir):
+    # pass_out generated with
+    # > echo -n ${{PASSWORD}} > ./pass
+    # > openssl aes-256-cbc -pbkdf2 -e -k "${{PYPI_PUBLISH_INDIRECT_KEY}}" < ./pass > ./pass_out
+    key = os.getenv('PYPI_PUBLISH_INDIRECT_KEY','')
+    if(key == ''):
+        raise Exception("missing env var PYPI_PUBLISH_INDIRECT_KEY")
+    path = os.path.join(root_dir, "pass")
+    path_out = os.path.join(root_dir, "..", "pass_out")
+    openssl_cmd = f'openssl aes-256-cbc -pbkdf2 -d -k "{key}" < {path_out} > {path}'
+    subprocess.call(openssl_cmd, cwd=root_dir, shell=True)
+    with open(path, 'r') as f:
+        password = f.read()
+    cmd = f"python3 -m twine upload --repository testpypi dist/* -u jenkins-stx -p '{password}'"
+    print(cmd)
+    os.remove(path)
+    subprocess.call(cmd, cwd=root_dir, shell=True)
+
+def main():
+    root = os.path.dirname(os.path.realpath(__file__))
+    src_dir = os.path.join(root, "src")
+    out_dir = os.path.join(root, "python")
+    stx_dir = os.path.join(out_dir, "systemathics")
+    dist_dir = os.path.join(out_dir, "dist")
+
+    print("Clean old generated code")
+    create_dir_if_needed(out_dir)
+    delete_dir_if_needed(stx_dir)
+    delete_dir_if_needed(dist_dir)
+
+    # get proto files (grpc and regular)
+    print("Sort proto files")
+    result = sort_proto_files(src_dir)
+    # generate new python file from proto
+    print("Generate proto files")
+    generate(result, src_dir, out_dir)
+
+    # add proto import to __init__.py files
+    print("Add namespace import to __init__.py files")
+    add_namespace_import_to_init_py(stx_dir)
+    
+    print("Replace version in setup")
+    replace_version_in_setup(out_dir)
+    print("Generate package")
+    generate_package(out_dir)
+    print("Publish package")
+    publish_package(out_dir)
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
---
+++ python/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name="systemathics.apis",
+    version="0.0.35",
+    author="Systemathics",
+    author_email="contact@systemathics.com",
+    description="Python grpc stub for Systemathics APIs.",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    namespace_packages=['systemathics'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=['googleapis-common-protos', 'protobuf', 'grpcio'],
+)
+
+## pip install --no-binary=protobuf protobuf
