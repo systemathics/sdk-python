d6cc77bf50ea01c627aabedb6885021596a52c86
David Denis <david.denis@systemathics.com>
2022-09-06 18:00:12 +0200

    Added description to pypi package

--- python/setup.py
+++ python/setup.py
@@ -3,9 +3,13 @@ import setuptools
 
 from setuptools import setup, find_packages
 
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst'), encoding='utf-8') as f:
+    lines = f.readlines()
+    description_content = "".join(lines)
+
 setup(
     name="systemathics.apis",
-    version="0.0.0",
+    version="1.0.0",
     author="Systemathics",
     author_email="contact@systemathics.com",
     description="Python grpc stub for Systemathics APIs.",
@@ -23,4 +27,6 @@ setup(
     license='MIT',
     python_requires='>=3.6',
     install_requires=['googleapis-common-protos', 'protobuf', 'grpcio'],
+    long_description= description_content,
+    long_description_content_type="text/x-rst; charset=UTF-8"
 )
