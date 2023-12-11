5b952ee538a4760e2e02581e5709ef40ce9040f2
David Denis <david.denis@systemathics.com>
2023-12-11 13:51:03 +0100

    Updated channel_helpers.py

--- src/systemathics/apis/helpers/channel_helpers.py
+++ src/systemathics/apis/helpers/channel_helpers.py
@@ -8,46 +8,53 @@ functions:
 """
 
 import os
-from shutil import ExecError
 import grpc
-
+import urllib.request
+import pathlib
+    
 DEFAULT_ENDPOINT = "https://grpc.ganymede.cloud"
 
-def get_grpc_channel() -> grpc.Channel:
+def get_grpc_channel(endpoint = "") -> grpc.Channel:
     """
     Get a channel suitable to call Ganymede gRPC API.
-    This uses the GRPC_APIS environment variable in the form http[s]://fdqn[:port] (if no scheme is give, we'll assume https).
-    If none is detected, use DEFAULT_ENDPOINT.
+    If no endpoint parameter is given, try to use GRPC_APIS environment variable or fallback to DEFAULT_ENDPOINT.
     Note:
-        For secure channels, we'll try to guess the path of CA certificates chain automatically.
-        For windows you need to 'pip install wheel python-certifi-win32' for that to work (it exports Windows CA Store to a PEM file).
+        For secure channels, we'll try to guess the path of CA certificates chain automatically, or download one from Mozilla official site.
         In the event CA certificates cannot be found, or if you want to use a custom file, set the SSL_CERT_FILE environment variable.
     Returns:
-        An aio channel suitable to call Ganymede gRPC API.
+        A channel suitable to call Ganymede gRPC API.
     """
-    endpoint = os.getenv('GRPC_APIS','')
+    endpoint = endpoint if endpoint else os.getenv('GRPC_APIS','')
     endpoint = endpoint if endpoint else DEFAULT_ENDPOINT # if no endpoint was provided, use the default one
     endpoint = endpoint if endpoint.startswith("http") else f"https://{endpoint}" # if no scheme was provided, assume it's https
+    return _get_grpc_channel(endpoint)
+    
+def _get_grpc_channel(endpoint: str) -> grpc.Channel:
+    if not endpoint or not endpoint.startswith("http"):
+        raise "endpoint should have scheme http or https"
     if (endpoint.startswith("https")):
         return grpc.secure_channel(endpoint.replace("https://",""), _get_channel_credentials())
     else:
         return grpc.insecure_channel(endpoint.replace("http://",""))
 
-def get_aio_grpc_channel() -> grpc.aio.Channel:
+def get_aio_grpc_channel(endpoint = "") -> grpc.aio.Channel:
     """
     Get an aio channel suitable to call Ganymede gRPC API.
-    This uses the GRPC_APIS environment variable in the form http[s]://fdqn[:port].
-    If none is detected, use DEFAULT_ENDPOINT.
+    If no endpoint parameter is given, try to use GRPC_APIS environment variable or fallback to DEFAULT_ENDPOINT.
     Note:
-        For secure channels, we'll try to guess the path of CA certificates chain automatically.
-        For windows you need to 'pip install wheel python-certifi-win32' for that to work (it exports Windows CA Store to a PEM file).
+        For secure channels, we'll try to guess the path of CA certificates chain automatically, or download one from Mozilla official site.
         In the event CA certificates cannot be found, or if you want to use a custom file, set the SSL_CERT_FILE environment variable.
     Returns:
         An aio channel suitable to call Ganymede gRPC API.
     """
-    endpoint = os.getenv('GRPC_APIS','')
+    endpoint = endpoint if endpoint else os.getenv('GRPC_APIS','')
     endpoint = endpoint if endpoint else DEFAULT_ENDPOINT # if no endpoint was provided, use the default one
     endpoint = endpoint if endpoint.startswith("http") else f"https://{endpoint}" # if no scheme was provided, assume it's https
+    return _get_aio_grpc_channel(endpoint)
+
+def _get_aio_grpc_channel(endpoint: str) -> grpc.aio.Channel:
+    if not endpoint or not endpoint.startswith("http"):
+        raise "endpoint should have scheme http or https"
     if (endpoint.startswith("https")):
         return grpc.aio.secure_channel(endpoint.replace("https://",""), _get_channel_credentials())
     else:
@@ -68,6 +75,29 @@ def _get_channel_credentials() -> grpc.ChannelCredentials:
         credentials = grpc.ssl_channel_credentials(f.read())
         return credentials
 
+def _get_current_mozilla_cacert() -> str:
+    # up to date CA bundle (the official one embedded in Mozilla)
+    url = "http://curl.haxx.se/ca/cacert.pem"
+    
+    # local path for CA bundle
+    cadir = os.path.join(str(pathlib.Path.home()), ".systemathics")
+    cafile = os.path.join(cadir, "cacert.pem")
+    if not os.path.exists(cadir):
+        os.makedirs(cadir)
+    
+    # don't redownload
+    if os.path.exists(cafile):
+        return cafile
+    
+    try:
+        with urllib.request.urlopen(url) as input:
+            with open(cafile, 'wb') as output:
+                output.write(input.read())
+    except urllib.error.URLError as e:
+        print(f"Could not get {url}: {e.reason}")
+           
+    return cafile
+
 def _autodetect_ca_bundle() -> str:
     cabundles = [
 	"/etc/ssl/certs/ca-certificates.crt",                                  # Debian/Ubuntu/Gentoo/etc..
@@ -76,11 +106,16 @@ def _autodetect_ca_bundle() -> str:
 	"/etc/pki/tls/cacert.pem",                                             # OpenELEC
 	"/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem",                   # CentOS/RHEL 7
 	"/etc/ssl/cert.pem",                                                   # Alpine Linux
-    os.path.join(os.getenv('LOCALAPPDATA',''), '.certifi', 'cacert.pem')   # Windows (requires: pip install wheel python-certifi-win32)
+    os.path.join(os.getenv('LOCALAPPDATA',''), '.certifi', 'cacert.pem')   # Windows (for those who installed python-certifi-win32, now defunct, e.i: pip install wheel python-certifi-win32)
     ]
 
+    # probe
     for cabundle in cabundles:
         if (os.path.isfile(cabundle)):
+            print(f"Using CA bundle {cabundle}")
             return cabundle
 
-    raise Exception(f"Could not auto detect trusted root certificates file, tried {cabundles}. Please help by setting SSL_CERT_FILE environment variable")
+    # fallback to current mozilla trusted root CA certificate chain
+    cabundle = _get_current_mozilla_cacert()
+    print(f"Using CA bundle {cabundle}")
+    return cabundle
